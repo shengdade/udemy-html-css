736890d9d2ce8e17705feb3aad87c4a06279d164
Dade <dasheng@amazon.com>
2019-01-12 16:48:39 -0500

    65. Scrolling to elements

--- Omnifood/Omnifood-Dade/resources/js/scripts.js
+++ Omnifood/Omnifood-Dade/resources/js/scripts.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  /* For the sticky navigation */
   $(".js--section-features").waypoint(
     function(direction) {
       if (direction == "down") {
@@ -11,4 +12,47 @@ $(document).ready(function() {
       offset: "60px"
     }
   );
+
+  /* Scroll on buttons */
+  $(".js--scroll-to-plan").click(function() {
+    $("html, body").animate(
+      {
+        scrollTop: $(".js--section-plans").offset().top
+      },
+      1000
+    );
+  });
+  $(".js--scroll-to-start").click(function() {
+    $("html, body").animate(
+      {
+        scrollTop: $(".js--section-features").offset().top
+      },
+      1000
+    );
+  });
+
+  /* Navigation scroll */
+  $(function() {
+    $("a[href*=#]:not([href=#])").click(function() {
+      if (
+        location.pathname.replace(/^\//, "") ==
+          this.pathname.replace(/^\//, "") &&
+        location.hostname == this.hostname
+      ) {
+        var target = $(this.hash);
+        target = target.length
+          ? target
+          : $("[name=" + this.hash.slice(1) + "]");
+        if (target.length) {
+          $("html,body").animate(
+            {
+              scrollTop: target.offset().top
+            },
+            1000
+          );
+          return false;
+        }
+      }
+    });
+  });
 });
